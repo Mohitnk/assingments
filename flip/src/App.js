b642a74f13cfc4045f4daf6d9454de970d506602
mohitnk <mohitsinghnk@gmail.com>
2022-05-03 10:51:47 +0530

    e-com

---
+++ flip/src/App.js
@@ -0,0 +1,13 @@
+ import logo from './logo.svg';
+ import './App.css';
+ import Figma from "./web/Figma";
+
+function App() {
+  return (
+  <div>
+    <Figma/>
+  </div>
+  );
+}
+
+export default App;
